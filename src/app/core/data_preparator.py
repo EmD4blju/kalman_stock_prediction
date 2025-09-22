aae222b2556e443b5ea4146b303476f8129deb15
Mikołaj Warda <mikolaj.warda@gmail.com>
2025-09-22 12:08:55 +0200

    feat: Added enhanced, supervised dataset preparation that adds RSI, Bandwidth and %B features if those exist in passed dataframe

--- src/app/core/data_preparator.py
+++ src/app/core/data_preparator.py
@@ -10,10 +10,12 @@ class DataPreparator():
     
     @staticmethod
     def reformat_periodic_to_supervised_data(dataframe:pd.DataFrame, target_column:str = 'Close', t:int = 2) -> pd.DataFrame:
-        """Reformats **periodic stock market** ```DataFrame``` to **supervised learning** ```DataFrame```.
+        """Reformats **periodic stock market** ```DataFrame``` to **supervised learning** ```DataFrame```. 
+        If the passed ```DataFrame``` contains technical indicators: 'RSI', 'Bandwidth' and '%B', those will be added to the reformatted ```DataFrame```.
 
         Args:
             dataframe (pd.DataFrame): A ```DataFrame``` to reformat.
+            target_column (str, optional): The target column to reformat. Defaults to 'Close'.
             t (int): Defines how many steps should the algorithm lookback.
 
         Returns:
@@ -36,6 +38,14 @@ class DataPreparator():
             previous_items = data_array[i-t:i][::-1]
             supervised_dataframe.loc[i-t] = [current_date, current_item, *previous_items]
         supervised_dataframe = supervised_dataframe.set_index(keys=['Date'])
+        
+        #~ --- Enhance ---
+        if {'RSI', 'Bandwidth', '%B'}.issubset(dataframe.columns):
+            log.info('Adding technical indicators to supervised data')
+            supervised_dataframe = supervised_dataframe.join(dataframe[['RSI', 'Bandwidth', '%B']], how='left')
+            
+        
+        
         
         #~ --- Return ---
         return supervised_dataframe
